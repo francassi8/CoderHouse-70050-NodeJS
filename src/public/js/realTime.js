4131a8836d457e76507518305fe11542b225db95
Franco Cassi <franco.cassi@adp.com>
2024-07-23 12:08:50 -0300

    fix de agregar producto, added eliminar producto

--- src/public/js/realTime.js
+++ src/public/js/realTime.js
@@ -3,15 +3,21 @@ const socket = io();
 const contenedorListaProductos = document.querySelector('#listaDeProductos')
 
 socket.on('realTimeProducts', (data) => {
-    contenedorListaProductos.innerHTML = ''
+
+    //Lista de Productos
+
+    contenedorListaProductos.innerHTML = '<h2>Lista de Productos</h2>'
     data.forEach(producto => {
         const div = document.createElement('div')
+        const idNode = document.createTextNode(`Id: ${producto.id}`);
         const titleNode = document.createTextNode(`Title: ${producto.title}`);
         const codeNode = document.createTextNode(`Code: ${producto.code}`);
         const priceNode = document.createTextNode(`Price: ${producto.price}`);
         const stockNode = document.createTextNode(`Stock: ${producto.stock}`);
         const statusNode = document.createTextNode(`Status: ${producto.status}`);
 
+        div.appendChild(idNode);
+        div.appendChild(document.createElement('br'));
         div.appendChild(titleNode);
         div.appendChild(document.createElement('br'));
         div.appendChild(codeNode);
@@ -26,4 +32,113 @@ socket.on('realTimeProducts', (data) => {
 
         contenedorListaProductos.appendChild(div);
     });
+
+    //agregar Producto
+
+    const crearProducto = document.querySelector('#crearProducto')
+    crearProducto.innerHTML = ''
+
+    const form = document.createElement('form');
+    form.id = 'productForm';
+
+        // Add HTML for the form fields
+        form.innerHTML = `
+            <div style="margin-left: 15px;">
+                <h2>Crear Producto</h2>
+                <label for="title">Title:</label><br>
+                <input type="text" id="title" name="title" required><br><br>
+                <label for="description">Description:</label><br>
+                <input type="text" id="description" name="description" required><br><br>
+                <label for="code">Code:</label><br>
+                <input type="text" id="code" name="code" required><br><br>
+                <label for="price">Price:</label><br>
+                <input type="number" id="price" name="price" required><br><br>
+                <label for="stock">Stock:</label><br>
+                <input type="number" id="stock" name="stock" required><br><br>
+                <label for="category">Category:</label><br>
+                <input type="text" id="category" name="category" required><br><br>
+                <button type="submit">Agregar Producto</button>
+            </div>
+        `;
+
+        crearProducto.appendChild(form);
+
+        form.addEventListener('submit', async function(event) {
+            event.preventDefault(); 
+            const formData = new FormData(form);
+            const data = {};
+            formData.forEach((value, key) => {
+                data[key] = value;
+            });
+            console.log(JSON.stringify(data))
+            try {
+                const response = await fetch('/api/products/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
+    
+                const result = await response.json();
+                alert(result.message);
+                form.reset();
+            } catch (error) {
+                console.error('Error:', error);
+                alert('Error al agregar el Producto');
+            }
+        });
+
+        //eliminar Producto
+
+        const eliminarProducto = document.querySelector('#eliminarProducto')
+        eliminarProducto.innerHTML = ''
+
+        const form_eliminar = document.createElement('div');
+        //form_eliminar.id = 'deleteForm';
+
+        form_eliminar.innerHTML = `
+            <div style="margin-left: 15px;">
+                <h2>Eliminar Producto</h2>
+                <label for="id_producto">producto ID:</label><br>
+                <input type="text" id="id_producto" name="id_producto" required><br><br>
+                <button class="deleteButton">Eliminar Producto</button>
+            </div>
+        `;
+
+        eliminarProducto.appendChild(form_eliminar);
+
+        document.addEventListener('click', async function(event) {
+            if (event.target.matches('.deleteButton')) {
+                event.preventDefault();
+                const productId = document.getElementById('id_producto').value;
+    
+                try {
+                    const response = await fetch(`/api/products/${productId}`, {
+                        method: 'DELETE',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+    
+                    if (response.ok) {
+                        const result = await response.json();
+                        alert(result.message); // Show success message
+    
+                        const productItem = document.querySelector(`.productItem[data-id="${productId}"]`);
+                        if (productItem) {
+                            productItem.remove();
+                        }
+                    } else {
+                        const result = await response.json();
+                        alert(result.error);
+                    }
+                } catch (error) {
+                    console.error('Error:', error);
+                    alert('Ingrese un ID de producto');
+                }
+            }
+        });
+        
+
 })
